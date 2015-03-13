ae97ce1b4b21da99e9780ac5f814e9d18d5faef2
Carl <carl@lift3d.co>
2015-03-13 01:23:33 -0400

    largely finished

--- src/html/app.js
+++ src/html/app.js
@@ -1,9 +1,37 @@
-require('./angular');
+var googleApp = angular.module('googleBooks', []);
 
-var app = angular.module('googleBooks', []);
+googleApp.controller('GoogleSearchCtrl',['$scope', '$http',function ($scope, $http) {
+    $scope.searchText = 'Hello World';
+    $scope.pageNumber = 1;
 
-app.controller('searchCtrl'[function($scope){
-    $scope.results = [];
+    $scope. maxResults = 10;
+
+    $scope.search = function () {
+
+        $scope.results = [];
+
+        $http({
+            method: 'GET',
+            url: "https://www.googleapis.com/books/v1/volumes",
+            params: {
+                q: $scope.searchText,
+                maxResults: $scope.maxResults,
+                startIndex: ($scope.pageNumber - 1) * $scope.maxResults
+            }
+        }).success(function (data) {
+
+                for( var i = 0; i < data.items.length; i++){
+                     books = data.items[i].volumeInfo;
+
+                    $scope.results.push(data.items[i].volumeInfo);
+                }
+            })
+    };
+
+    $scope.changePage = function (increment) {
+        $scope.pageNumber += increment;
+
+    };
 
 
 }]);
